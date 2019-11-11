c648c9b6fdb840ac855ed33e35e2d36cf042ca3a
matjanko <mateusz.jankowski@prokonto.pl>
2019-11-11 23:29:48 +0100

    Create 'project name' data transfer object

---
+++ src/main/java/pl/tekprojekt/teksystem/reports/dto/ProjectNameDto.java
@@ -0,0 +1,16 @@
+package pl.tekprojekt.teksystem.reports.dto;
+
+
+import lombok.Value;
+
+/**
+ * @author matjanko
+ *
+ */
+
+@Value
+public class ProjectNameDto {
+
+    private Long id;
+    private String fullName;
+}
