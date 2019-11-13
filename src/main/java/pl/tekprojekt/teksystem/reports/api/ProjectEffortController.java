fc52808a8dc779a540419b64cd98c5be7377fdf7
matjanko <mateusz.jankowski@prokonto.pl>
2019-11-13 23:11:18 +0100

    Create project effort api controller

---
+++ src/main/java/pl/tekprojekt/teksystem/reports/api/ProjectEffortController.java
@@ -0,0 +1,37 @@
+package pl.tekprojekt.teksystem.reports.api;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.tekprojekt.teksystem.reports.dto.ProjectEffortDto;
+import pl.tekprojekt.teksystem.reports.service.ProjectEffortService;
+
+import java.util.List;
+
+/**
+ * @author matjanko
+ *
+ */
+
+@RestController
+@RequestMapping("/projects/efforts")
+public class ProjectEffortController {
+
+    private ProjectEffortService projectEffortService;
+
+    public ProjectEffortController(ProjectEffortService projectEffortService) {
+        this.projectEffortService = projectEffortService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProjectEffortDto>> getAllProjectEfforts() {
+        List<ProjectEffortDto> projectEfforts = projectEffortService.findAllProjectEffort();
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(projectEfforts);
+    }
+
+}
