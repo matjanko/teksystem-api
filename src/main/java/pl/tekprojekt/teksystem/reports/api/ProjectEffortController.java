2021fd217dd3fd77206feef67ab5f1604397c6d1
matjanko <mateusz.jankowski@prokonto.pl>
2019-11-15 00:43:05 +0100

    Add cross origin annotation to project effort api controller

--- src/main/java/pl/tekprojekt/teksystem/reports/api/ProjectEffortController.java
+++ src/main/java/pl/tekprojekt/teksystem/reports/api/ProjectEffortController.java
@@ -2,6 +2,7 @@ package pl.tekprojekt.teksystem.reports.api;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ import java.util.List;
  */
 
 @RestController
+@CrossOrigin
 @RequestMapping("/projects/efforts")
 public class ProjectEffortController {
 
