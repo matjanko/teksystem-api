0fa0941a0f1e4b6dd69cbc10eeaee3d4c8d09d67
matjanko <mateusz.jankowski@prokonto.pl>
2020-02-17 18:53:53 +0100

    Initial commit of employer microservice

---
+++ teksystem-employer-service/src/main/java/com/github/matjanko/teksystem/employers/EmployersServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.matjanko.teksystem.employers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployersServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployersServiceApplication.class, args);
+	}
+
+}
---
+++ teksystem-employer-service/src/test/java/com/github/matjanko/teksystem/employers/EmployersServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.matjanko.teksystem.employers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployersServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
