829d4c5e8666f0a1ef4c3f550be8b26f46c036b9
matjanko <mateusz.jankowski@prokonto.pl>
2020-02-20 21:50:03 +0100

    Get all role names implementation

--- teksystem-employee-service/src/main/java/com/github/matjanko/teksystem/employee/api/EmployeeRoleController.java
+++ teksystem-employee-service/src/main/java/com/github/matjanko/teksystem/employee/api/EmployeeRoleController.java
@@ -1,5 +1,8 @@
 package com.github.matjanko.teksystem.employee.api;
 
+import com.github.matjanko.teksystem.employee.services.EmployeeRoleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,10 +17,15 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/role")
+@RequiredArgsConstructor
 public class EmployeeRoleController {
+
+    private final EmployeeRoleService roleService;
 
     @GetMapping(produces = "application/json")
     public ResponseEntity<List<String>> getAllEmployeeRoleNames() {
-        return null;
+        List<String> roleNames = roleService.getAllRoleNames();
+        return new ResponseEntity<>(roleNames, HttpStatus.OK);
     }
+
 }
