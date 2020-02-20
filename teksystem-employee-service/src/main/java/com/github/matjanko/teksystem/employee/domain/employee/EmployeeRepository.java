eb5c5252a8f3647437df39ebbbfd74c01c01ddd1
matjanko <mateusz.jankowski@prokonto.pl>
2020-02-20 23:46:33 +0100

    Added method to Employee repo

--- teksystem-employee-service/src/main/java/com/github/matjanko/teksystem/employee/domain/employee/EmployeeRepository.java
+++ teksystem-employee-service/src/main/java/com/github/matjanko/teksystem/employee/domain/employee/EmployeeRepository.java
@@ -3,11 +3,14 @@ package com.github.matjanko.teksystem.employee.domain.employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import javax.validation.constraints.NotBlank;
+import java.util.List;
 
 /**
  * @author matjanko
  */
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    List<Employee> findAllByIsActiveIsTrue();
 
     Employee findByUsername(@NotBlank String username);
 
